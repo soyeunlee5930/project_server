cddbf799e24fa0aeab365a1e110accb5410f34df
soyeunlee <soyeun.lee5930@gmail.com>
2024-03-14 15:29:22 +0900

    feat : category delete 시, 연관된 subCategory delete 처리하기

--- src/main/java/project/project/mapper/SubCategoriesMapper.java
+++ src/main/java/project/project/mapper/SubCategoriesMapper.java
@@ -16,4 +16,7 @@ public interface SubCategoriesMapper {
     void updateSubCategory(SubCategories subCategory);
 
     void deleteSubCategory(Integer id);
+
+    // 메인 카테고리와 연관된 서브 카테고리 삭제
+    void deleteSubCategoriesByCategoryId(Integer id);
 }
--- src/main/java/project/project/service/CategoriesServiceImpl.java
+++ src/main/java/project/project/service/CategoriesServiceImpl.java
@@ -3,6 +3,7 @@ package project.project.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.project.mapper.CategoriesMapper;
+import project.project.mapper.SubCategoriesMapper;
 import project.project.model.Categories;
 
 import java.util.List;
@@ -11,6 +12,9 @@ import java.util.List;
 public class CategoriesServiceImpl implements CategoriesService {
     @Autowired
     private CategoriesMapper categoriesMapper;
+
+    @Autowired
+    private SubCategoriesMapper subCategoriesMapper;
 
     @Override
     public List<Categories> getAllCategories() {
@@ -34,6 +38,10 @@ public class CategoriesServiceImpl implements CategoriesService {
 
     @Override
     public void deleteCategory(Integer id) {
+        // 메인 카테고리에 연관된 모든 서브 카테고리 먼저 삭제
+        subCategoriesMapper.deleteSubCategoriesByCategoryId(id);
+
+        // 그 후 메인 카테고리 삭제
         categoriesMapper.deleteCategory(id);
     }
 }
